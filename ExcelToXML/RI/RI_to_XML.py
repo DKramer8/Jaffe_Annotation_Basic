50f65d614e1fefcd742dbf0dbf9807ac8a773635
DKramer8 <103896137+DKramer8@users.noreply.github.com>
2025-05-07 15:59:58 +0200

    Progress

--- ExcelToXML/RI/RI_to_XML.py
+++ ExcelToXML/RI/RI_to_XML.py
@@ -5,17 +5,39 @@ from bs4 import BeautifulSoup
 INPUT_PATH = "RI_test.xlsx"
 OUTPUT_PATH = "RI_Output.xml"
 COLUMNS_XML_MAP = { # "excel_column": "xml" 
-    "identifier": "idno", # Steht nicht so im Template
+    # "zählen": "",
+    "identifier": "idno",
     "locality_string": "place_name",
     "start_date": "date_notBefore",
     "end_date": "date_notAfter",
     "summary": "abstract_p",
-    "archival_history": "bibliography_list_bibl",
+    # "import": "", # Wahrscheinlich irrelevant
+    "archival_history": "historiography_list_bibl",
     "title": "bibl", # Plus den identifier; ist so in der Logik unten eingbaut    
     "commentary": "commentary_p",
-    "original_date": "date",
-    "recipient": "legal_actor_recipient_inner", # Steht nicht so im Template    
-    "urn": "idno_urn" # Steht nicht so im Template
+    "literature": "bibliography_list_bibl",
+    # "annotations": "", # Ist irgendwie in allen drei Excels leer
+    "incipit": "incipit_diploPart",
+    # "original_date": "date", # Was ist das? Scheint nicht das gleiche wie date_string zu sein
+    # "verso_note": "", # Ist irgendwie in allen drei Excels leer
+    # "seal": "", # Scheint mir wichtig, aber kein Platz im Template?
+    "recipient": "legal_actor_recipient_inner",
+    # "witnesses": "", # Zeuge scheint auch wichtig zu sein, aber kein Platz im Template?
+    #" clerk": "", # Scheint mir wichtig, aber kein Platz im Template?
+    # "chancellor": "", # Scheint mir wichtig, aber kein Platz im Template?
+    # "signature": "", # Ist irgendwie in allen drei Excels leer
+    # "signature_addition": "",	# Ist irgendwie in allen drei Excels leer
+    # "external_links": "", # Scheint mir wichtig, aber kein Platz im Template?
+    # "exchange_identifier": "", # Ist zumindest in de Excels gefüllt, aber keine Ahnung wie wichtig das ist
+    "urn": "idno_urn", 
+    # "pid": "", # Was ist das? 
+    # "uid": "", # Was ist das?
+    # "sorting": "", # Was ist das?
+    # "banpk": "", # Was ist das?
+    # "laufendenummer": "", # Brauchen wir wohl nicht
+    # "regeestennummernorm": "", # Was ist das?
+    # "persistent_identifier": "", # Was ist das?
+    "date_string": "date"
 }
 xml_content_map = { # "xml": "content"
     "title": "[Die Nummer des Regests im Projekt]",
@@ -70,60 +92,60 @@ def create_tei_xml(output_file):
     file_desc = etree.SubElement(tei_header, "fileDesc")
     title_stmt = etree.SubElement(file_desc, "titleStmt")
     title = etree.SubElement(title_stmt, "title")
-    title.text = xml_content_map["title"]
+    title.text = str(xml_content_map["title"])
 
     resp_stmt1 = etree.SubElement(title_stmt, "respStmt")
     resp1 = etree.SubElement(resp_stmt1, "resp")
-    resp1.text = xml_content_map["resp1"]
+    resp1.text = str(xml_content_map["resp1"])
     pers_name1 = etree.SubElement(resp_stmt1, "persName")
-    pers_name1.text = xml_content_map["pers_name1"]
+    pers_name1.text = str(xml_content_map["pers_name1"])
 
     resp_stmt2 = etree.SubElement(title_stmt, "respStmt")
     resp2 = etree.SubElement(resp_stmt2, "resp", attrib={"when": "2024"})
-    resp2.text = xml_content_map["resp2"]
+    resp2.text = str(xml_content_map["resp2"])
     pers_name2 = etree.SubElement(resp_stmt2, "persName")
-    pers_name2.text = xml_content_map["pers_name2"]
+    pers_name2.text = str(xml_content_map["pers_name2"])
 
     publication_stmt = etree.SubElement(file_desc, "publicationStmt")
     publisher = etree.SubElement(publication_stmt, "publisher")
-    publisher.text = xml_content_map["publisher"]
+    publisher.text = str(xml_content_map["publisher"])
     idno = etree.SubElement(publication_stmt, "idno")
-    idno.text = xml_content_map["idno"]
+    idno.text = str(xml_content_map["idno"])
     idno_urn = etree.SubElement(publication_stmt, "idno", attrib={"type": "urn"})
-    idno_urn.text = xml_content_map["idno_urn"]    
+    idno_urn.text = str(xml_content_map["idno_urn"])
     availability = etree.SubElement(publication_stmt, "availability")
     licence = etree.SubElement(availability, "licence")
-    licence.text = xml_content_map["licence"]
+    licence.text = str(xml_content_map["licence"])
 
     source_desc = etree.SubElement(file_desc, "sourceDesc")
     bibl = etree.SubElement(source_desc, "bibl")
-    bibl.text = f"{xml_content_map["bibl"]}, {xml_content_map["idno"]}"
+    bibl.text = f"{str(xml_content_map['bibl'])}, {str(xml_content_map['idno'])}"
     bibl_jaffe = etree.SubElement(source_desc, "bibl", attrib={"type": "jaffe"})
-    bibl_jaffe.text = xml_content_map["bibl_jaffe"]
+    bibl_jaffe.text = str(xml_content_map["bibl_jaffe"])
 
     diplo_desc = etree.SubElement(source_desc, "diploDesc")
     issued = etree.SubElement(diplo_desc, "p")
     issued_inner = etree.SubElement(issued, "issued")
     place_name = etree.SubElement(issued_inner, "placeName")
-    place_name.text = xml_content_map["place_name"]
+    place_name.text = str(xml_content_map["place_name"])
     date = etree.SubElement(issued_inner, "date", attrib={
         "type": "issued",
-        "notBefore": xml_content_map["date_notBefore"],
-        "notAfter": xml_content_map["date_notAfter"]
+        "notBefore": str(xml_content_map["date_notBefore"]),
+        "notAfter": str(xml_content_map["date_notAfter"])
     })
-    date.text = xml_content_map["date"]
+    date.text = str(xml_content_map["date"])
 
     legal_actor_issuer = etree.SubElement(diplo_desc, "p")
     legal_actor_issuer_inner = etree.SubElement(legal_actor_issuer, "legalActor", attrib={"type": "issuer"})
-    legal_actor_issuer_inner.text = xml_content_map["legal_actor_issuer_inner"]
+    legal_actor_issuer_inner.text = str(xml_content_map["legal_actor_issuer_inner"])
 
     legal_actor_recipient = etree.SubElement(diplo_desc, "p")
     legal_actor_recipient_inner = etree.SubElement(legal_actor_recipient, "legalActor", attrib={"type": "recipient"})
-    legal_actor_recipient_inner.text = xml_content_map["legal_actor_recipient_inner"]
+    legal_actor_recipient_inner.text = str(xml_content_map["legal_actor_recipient_inner"])
 
     object_type = etree.SubElement(diplo_desc, "p")
     object_type_inner = etree.SubElement(object_type, "objectType")
-    object_type_inner.text = xml_content_map["object_type_inner"]
+    object_type_inner.text = str(xml_content_map["object_type_inner"])
 
     # profileDesc
     profile_desc = etree.SubElement(tei_header, "profileDesc")
@@ -159,68 +181,68 @@ def create_tei_xml(output_file):
     # Abstract
     abstract_div = etree.SubElement(body, "div", attrib={"type": "abstract"})
     etree.SubElement(abstract_div, "head").text = "Abstract:"
-    etree.SubElement(abstract_div, "p").text = xml_content_map["abstract_p"]
+    etree.SubElement(abstract_div, "p").text = str(xml_content_map["abstract_p"])
 
     # Echtheitskriterien
     authen_div = etree.SubElement(body, "div", attrib={"type": "other"})
     etree.SubElement(authen_div, "head").text = "Echtheitskriterien:"
     authen = etree.SubElement(authen_div, "authen", attrib={"type": "formula"})
-    etree.SubElement(authen, "p").text = xml_content_map["authen_p"]
+    etree.SubElement(authen, "p").text = str(xml_content_map["authen_p"])
 
     # Incipit
     incipit_div = etree.SubElement(body, "div")
     etree.SubElement(incipit_div, "head").text = "Incipit:"
-    etree.SubElement(incipit_div, "diploPart", attrib={"type": "incipit", "{http://www.w3.org/XML/1998/namespace}lang": "lat"}).text = xml_content_map["incipit_diploPart"]
+    etree.SubElement(incipit_div, "diploPart", attrib={"type": "incipit", "{http://www.w3.org/XML/1998/namespace}lang": "lat"}).text = str(xml_content_map["incipit_diploPart"])
 
     # Datatio
     datatio_div = etree.SubElement(body, "div")
     etree.SubElement(datatio_div, "head").text = "Datatio:"
-    etree.SubElement(datatio_div, "diploPart", attrib={"type": "datatio"}).text = xml_content_map["datatio_diploPart"]
+    etree.SubElement(datatio_div, "diploPart", attrib={"type": "datatio"}).text = str(xml_content_map["datatio_diploPart"])
 
     # Handschriftliche Überlieferung
     ms_sources_div = etree.SubElement(body, "div", attrib={"type": "msSources"})
     etree.SubElement(ms_sources_div, "head").text = "Handschriftliche Überlieferung und alte Drucke"
     ms_list_bibl = etree.SubElement(ms_sources_div, "listBibl")
-    etree.SubElement(ms_list_bibl, "bibl").text = xml_content_map["ms_list_bibl"]
+    etree.SubElement(ms_list_bibl, "bibl").text = str(xml_content_map["ms_list_bibl"])
 
     # Dekretalen
     decretals_div = etree.SubElement(body, "div", attrib={"type": "decretals"})
     etree.SubElement(decretals_div, "head").text = "Dekretalen"
     decretals_list_bibl = etree.SubElement(decretals_div, "listBibl")
-    etree.SubElement(decretals_list_bibl, "bibl").text = xml_content_map["decretals_list_bibl"]
+    etree.SubElement(decretals_list_bibl, "bibl").text = str(xml_content_map["decretals_list_bibl"])
 
     # Historiography
     historiography_div = etree.SubElement(body, "div", attrib={"type": "historiography"})
     etree.SubElement(historiography_div, "head").text = "Erwähnungen in der Historiographie"
     historiography_list_bibl = etree.SubElement(historiography_div, "listBibl")
-    etree.SubElement(historiography_list_bibl, "bibl").text = xml_content_map["historiography_list_bibl"]
+    etree.SubElement(historiography_list_bibl, "bibl").text = str(xml_content_map["historiography_list_bibl"])
 
     # Editions
     editions_div = etree.SubElement(body, "div", attrib={"type": "editions"})
     etree.SubElement(editions_div, "head").text = "Editionen"
     editions_list_bibl = etree.SubElement(editions_div, "listBibl")
-    etree.SubElement(editions_list_bibl, "bibl").text = xml_content_map["editions_list_bibl"]
+    etree.SubElement(editions_list_bibl, "bibl").text = str(xml_content_map["editions_list_bibl"])
 
     # Regesta
     regesta_div = etree.SubElement(body, "div", attrib={"type": "regesta"})
     etree.SubElement(regesta_div, "head").text = "Regesten"
     regesta_list_bibl = etree.SubElement(regesta_div, "listBibl")
-    etree.SubElement(regesta_list_bibl, "bibl").text = xml_content_map["regesta_list_bibl"]
+    etree.SubElement(regesta_list_bibl, "bibl").text = str(xml_content_map["regesta_list_bibl"])
 
     # Commentary
     commentary_div = etree.SubElement(body, "div", attrib={"type": "commentary"})
     etree.SubElement(commentary_div, "head").text = "Sachkommentar"
-    etree.SubElement(commentary_div, "p").text = xml_content_map["commentary_p"]
+    etree.SubElement(commentary_div, "p").text = str(xml_content_map["commentary_p"])
 
     # Bibliography
     bibliography_div = etree.SubElement(body, "div", attrib={"type": "bibliography"})
     etree.SubElement(bibliography_div, "head").text = "Bibliographie"
     bibliography_list_bibl = etree.SubElement(bibliography_div, "listBibl")
-    etree.SubElement(bibliography_list_bibl, "bibl").text = xml_content_map["bibliography_list_bibl"]
+    etree.SubElement(bibliography_list_bibl, "bibl").text = str(xml_content_map["bibliography_list_bibl"])
 
     # Notes
     notes_div = etree.SubElement(body, "div", attrib={"type": "note"})
-    etree.SubElement(notes_div, "head").text = xml_content_map["notes_head"]
+    etree.SubElement(notes_div, "head").text = str(xml_content_map["notes_head"])
 
     # Write to file with processing instructions
     with open(output_file, "wb") as f:
@@ -232,7 +254,7 @@ def create_tei_xml(output_file):
 df = pd.read_excel(INPUT_PATH, header=0, dtype=str)
 for id, row in df.iterrows():
     for col, value in row.items():
-        if col in COLUMNS_XML_MAP and value != "nan":
+        if col in COLUMNS_XML_MAP and value != "nan" and value is not None:
             print(value)
             clean_value = remove_html_tags(value)
             print(clean_value)
